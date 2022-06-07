ff2330e13440692bccac9e71c98cffdab252192a
jihyelucky <94450652+jihyelucky@users.noreply.github.com>
2022-06-07 22:20:46 +0900

    style modify

--- js/todo.js
+++ js/todo.js
@@ -22,7 +22,7 @@ function paintTodo(newTodo) {
   const span = document.createElement("span");
   span.innerText = newTodo.text;
   const button = document.createElement("button");
-  button.innerText = "❌";
+  button.innerText = "✔️";
   button.addEventListener("click", deleteTodo);
   li.appendChild(span);
   li.appendChild(button);
